223cb71feb5ef2958683d350ca9abe2886c09b98
dmlicuanan <dmlicuanan@gmail.com>
2022-07-25 10:38:51 +0800

    Final versions of codes used for BOLD download

--- bold_download.R
+++ bold_download.R
@@ -1,10 +1,10 @@
 # this script is for downloading fasta files from BOLD
 # gene: COI 
-# species: marine eukaryotes
+# species: marine eukaryotes; species list used contained in "wormstaxlist" file
 # see https://docs.ropensci.org/bold/#large-data 
 
 # install packages
-install.packages("bold")
+# install.packages("bold")
 # load packages
 library(bold)
 
@@ -12,7 +12,7 @@ library(bold)
 # API key from amlicuanan account at https://www.ncbi.nlm.nih.gov/account/settings/
 # options(ENTREZ_KEY = "7d21c9bc180e2c7f4c118b010e6f236f3008")
 
-# set working directory to where wormstaxlist is located
+# set working directory to where wormstaxlist is located 
 setwd("D:/Documents/NGS/entrez/")
 # read-in taxa list
 wormstaxlist <- readLines("wormstaxlist", encoding = "UTF-8")
@@ -21,14 +21,27 @@ wormstaxlist <- wormstaxlist[order(wormstaxlist)]
 # remove "'" since it causes Internal Server Error (HTTP 500) in BOLD
 wormstaxlist <- gsub("\\'", "", wormstaxlist)
 # check other symbols/punctuations in list
-wormstaxlist[grep("[[:punct:]]", wormstaxlist)]
+# wormstaxlist[grep("[[:punct:]]", wormstaxlist)]
+
+# assign list of taxa
+list <- wormstaxlist
+rm(wormstaxlist)
 
 # create directory where fastas will be saved
 # dir.create("./bold_fastas")
 setwd("./bold_fastas")
 
-# function for getting fasta (modified from MARES)
-# version 1 used for 1:27203 in for-loop
+# note: 
+# a for-loop containing a custom function (get_fasta) was used to download BOLD sequences
+# both for-loop and get_fasta were modified during the run to remove errors
+# version 1 of codes (lines commented below) were used for species approx. indexed 1:27203 and 111161:112648
+# 1:27203 was run in laptop; 111161:112648 run in 172 PC remotely
+# version 2 of codes were used for the rest of the sequences
+# laptop run finished early, tail of wormstaxlist (around 215000:222320) was run in laptop instead of 172 PC
+
+#################################
+# version 1 of get_fasta function
+# modified from https://github.com/wpearman1996/MARES_database_pipeline/blob/master/step2_retrieve_bold.r
 # get_fasta <- function(taxon, filename) {
 #   x <- bold_seqspec(taxon = taxon, marker = c("COI-5P", "COI-3P"))
 #   x <- x[x$markercode == "COI-5P" | x$markercode == "COI-3P", ]
@@ -54,9 +67,30 @@ setwd("./bold_fastas")
 #   writeLines(fastaLines, filename)
 # }
 
-# function for getting fasta (modified from MARES)
-# version 2 used for 27204 in for-loop
-# removed marker argument in bold_seqspec
+# version 1 of for-loop
+# log.txt for laptop run; log_172.txt for 172 PC run
+# sink("log.txt", append = TRUE) 
+# for(i in 1:length(list)) {
+#   # check if taxon in BOLD before proceeding
+#   taxon <- list[i]
+#   x <- bold_tax_name(taxon)
+#   
+#   # only proceed if taxon is in BOLD 
+#   if(dim(x)[2] > 2) {
+#     # progress
+#     cat("Processing", taxon, ":", i, "of", length(list), "\n")
+#     
+#     tryCatch({
+#       get_fasta(taxon = taxon, filename = paste0(gsub(" ", "_", taxon), "_bold.fasta"))
+#     }, error = function(e) {cat("ERROR :", conditionMessage(e), "\n")})
+#   } else { NULL }
+# }
+# sink()
+#################################
+
+# version 2 of get_fasta function
+# removed marker argument in bold_seqspec and stopped process if object is atomic (just NA) and marker is not COI-5P or COI-3P
+# https://github.com/ropensci/bold/issues/76
 get_fasta <- function(taxon, filename) {
   x <- bold_seqspec(taxon = taxon)
   
@@ -85,59 +119,16 @@ get_fasta <- function(taxon, filename) {
   } else { NULL }
 }
 
-# assign list of taxa
-list <- wormstaxlist
-rm(wormstaxlist)
-
-# 1) for laptop run (1st half of species list)
+# version 2 of for-loop
+# log.txt for laptop run; log_172.txt for 172 PC run
 sink("log.txt", append = TRUE)
-for(i in 27204:111160) {
-  # check if taxon in BOLD before proceeding
-  taxon <- list[i]
-  x <- bold_tax_name(taxon)
-  
-  # only proceed if taxon is in BOLD 
-  if(dim(x)[2] > 2) {
-    # progress
-    cat("Processing", taxon, ":", i, "of", length(list), "\n")
-    
-    tryCatch({
-      get_fasta(taxon = taxon, filename = paste0(gsub(" ", "_", taxon), "_bold.fasta"))
-    }, error = function(e) {cat("ERROR :", conditionMessage(e), "\n")})
-  } else { NULL }
-}
-sink()
-
-# 2) for 172 PC run (start at 2nd half of species list)
-sink("log_172.txt", append = TRUE)
-for(i in 111161:length(list)) {
-  # check if taxon in BOLD before proceeding
-  taxon <- list[i]
-  x <- bold_tax_name(taxon)
-  
-  # only proceed if taxon is in BOLD 
-  if(dim(x)[2] > 2) {
-    # progress
-    cat("Processing", taxon, ":", i, "of", length(list), "\n")
-    
-    tryCatch({
-      get_fasta(taxon = taxon, filename = paste0(gsub(" ", "_", taxon), "_bold.fasta"))
-    }, error = function(e) {cat("ERROR :", conditionMessage(e), "\n")})
-  } else { NULL }
-}
-sink()
-
-
-################
-# revised code for laptop # 27204
-sink("log.txt", append = TRUE)
-for(i in 35986:111160) {
+for(i in 1:length(list)) {
   # check if taxon in BOLD before proceeding
   taxon <- list[i]
   x <- bold_tax_name(taxon)
   s <- bold_stats(taxon)
   
-  # only proceed if taxon is in BOLD 
+  # only proceed if taxon is in BOLD and there are records
   if(dim(x)[2] > 2 & s$total_records > 1) {
     # progress
     cat("Processing", taxon, ":", i, "of", length(list), "\n")
@@ -149,23 +140,3 @@ for(i in 35986:111160) {
 }
 sink()
 
-
-# revised for 172 PC:
-sink("log_172.txt", append = TRUE)
-for(i in 112648:length(list)) {
-  # check if taxon in BOLD before proceeding
-  taxon <- list[i]
-  x <- bold_tax_name(taxon)
-  s <- bold_stats(taxon)
-  
-  # only proceed if taxon is in BOLD 
-  if(dim(x)[2] > 2 & s$total_records > 1) {
-    # progress
-    cat("Processing", taxon, ":", i, "of", length(list), "\n")
-    
-    tryCatch({
-      get_fasta(taxon = taxon, filename = paste0(gsub(" ", "_", taxon), "_bold.fasta"))
-    }, error = function(e) {cat("ERROR :", conditionMessage(e), "\n")})
-  } else { NULL }
-}
-sink()
