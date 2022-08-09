792a9e70d5d516c1405dae5876b7b7a6444f2115
dmlicuanan <dmlicuanan@gmail.com>
2022-08-09 11:51:44 +0800

    Included subset runs, extracting species that need to be re-run from log files

--- ncbi_download.sh
+++ ncbi_download.sh
@@ -121,7 +121,7 @@ time parallel -j 10 --delay 1.1 mydownload :::: subset 2>&1 | tee -a log_ncbi.tx
 
 
 
-########## version 2 -- last edit 29/07/2022
+########## version 2 for running in Sir's machine -- last edit 29/07/2022
 
 # set working directory (OneDrive)
 cd /mnt/c/Users/Ardea\ Licuanan/OneDrive/2022_kraken/
@@ -171,3 +171,158 @@ export -f mydownload
 # run in parallel (10 jobs at a time, delayed start of 1.1 seconds)
 # save errors to log_ncbi.txt
 time parallel -j 10 --delay 1.1 mydownload :::: wormstaxlist_fin 2>&1 | tee -a log_ncbi.txt
+
+
+
+
+########## version for "back-up run" from 2022/08/01 to 2022/08/06
+# set working directory 
+cd /mnt/d/Documents/NGS/entrez/
+
+# use API key of amlicuanan@alum.up.edu.ph 
+export NCBI_API_KEY=81af93ade140a45a355b621d22a8692a5408
+
+# create directory where GenBank files will be saved:
+# mkdir wormstaxlist_gb
+
+# check taxa with special characters 
+# all special characters must be URL encoded -- https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESearch
+# grep -v "^[a-zA-Z -.]*$" wormstaxlist 
+
+# remove strings [sensu lato] and (incertae sedis) + characters × and ,
+# delete lines with [non-Uristidae]
+# convert all characters to ascii
+# remove single and double quotes
+# sed -e "s/\( \[sensu lato\]\| (incertae sedis)\|[×,]\)//" wormstaxlist | sed '/\[non-Uristidae\]/d' | iconv -f utf-8 -t ascii//translit | tr -d \'\" > wormstaxlist_cleaned
+# check if all special characters are removed
+# grep -v "^[a-zA-Z -.]*$" wormstaxlist_cleaned 
+
+# add FIELDS to each line in taxa list
+# sed 's/$/ [ORGN] AND (CO1 [GENE] OR COI [GENE] OR COX1 [GENE] OR COXI [GENE])/' wormstaxlist_cleaned > wormstaxlist_fin
+
+# create function for downloading GB flat file
+mydownload() {
+query="$1"
+IFS=$'\n'
+search=$(esearch -db nuccore -query "$query") 
+count=$(echo "$search" | xtract -pattern Count -element Count)
+if [ $count -gt 0 ]
+then 
+filename=$(echo "$query" | sed 's/ /_/;s/\[.*//;s/ //')
+echo "$search" | efetch -format gb > ./wormstaxlist_gb/${filename}_ncbi.gb
+fi	
+}
+export -f mydownload
+
+# create subsets of 10,000 and put in separate directory
+# mkdir wormstaxlist_gb/subsets
+# split --verbose -l10000 wormstaxlist_fin ./wormstaxlist_gb/subsets/subset.
+
+# run in parallel (10 jobs at a time, delayed start of 1.1 seconds)
+# save errors to log_ncbi.txt appended by subset identifier
+# change subset files manually
+time parallel -j 10 --delay 1.1 mydownload :::: ./wormstaxlist_gb/subsets/subset.ab 2>&1 | tee -a ./wormstaxlist_gb/subsets/log_ncbi_ab.txt 
+# 8/1/2022
+# subset.aa 10:04 AM (186m34.434s)
+# subset.ab 1:27 PM (95m51.774s) only 1612 downloaded; re-run on 8/8/2022 6:04 PM (197m39.768s) 2782 downloaded after re-run
+# subset.aw 6:26 PM (45m14.437s)
+# subset.ac 7:19 PM (195m9.809s)
+# subset.ad 10:42 PM (195m8.691s)
+# 8/2/2022
+# subset.ae 9:19 AM (207m3.172s)
+# subset.af 5:28 PM (194m25.961s)
+# subset.ag 8:54 PM (195m13.427s)
+# 8/3/2022
+# subset.ah 9:22 AM (195m59.597s)
+# subset.ai 5:16 PM (194m30.055s)
+# subset.aj 8:53 PM (194m25.263s)
+# subset.ak 12:14 AM (193m20.197s)
+# 8/4/2022
+# subset.al 9:48 AM (194m31.344s)
+# subset.am 1:11 PM (194m51.515s)
+# subset.an 5:49 PM (193m40.201s)
+# subset.ao 9:40 PM (193m18.177s)
+# 8/5/2022
+# subset.ap 9:12 AM (194m50.979s)
+# subset.aq 12:40 PM (194m52.813s)
+# subset.ar 5:40 PM (193m2.918s)
+# subset.as 10:10 (192m59.591s)
+# 8/6/2022
+# subset.at 8:13 AM (193m27.705s)
+# subset.au 12:15 PM (193m13.827s)
+# subset.av 3:57 PM (193m15.131s)
+
+# 2022/08/08: check log files for species that need to be re-run
+cd /mnt/d/Documents/NGS/entrez/wormstaxlist_gb/subsets/
+# concatenate log files into a single file
+cat *.txt >> log_ncbi_compiled.txt
+
+# match string between -term and [ORGN] in log files
+# replace underscore with space and append other fields
+# https://stackoverflow.com/questions/13242469/how-to-use-sed-grep-to-extract-text-between-two-words
+grep -o -P '(?<= -term ").*?(?= \[ORGN\])' log_ncbi_compiled.txt | sed 's/_/ /g' | sed 's/$/ [ORGN] AND (CO1 [GENE] OR COI [GENE] OR COX1 [GENE] OR COXI [GENE])/' | sort -u > /mnt/d/Documents/NGS/entrez/wormstaxlist_rerun
+# re-run for species in log_ncbi_ab.txt (after 2022/08/08 re-run)
+grep -o -P '(?<= -term ").*?(?= \[ORGN\])' log_ncbi_ab.txt | sed 's/_/ /g' | sed 's/$/ [ORGN] AND (CO1 [GENE] OR COI [GENE] OR COX1 [GENE] OR COXI [GENE])/' | sort -u > /mnt/d/Documents/NGS/entrez/wormstaxlist_rerun_ab
+
+# perform re-run -- 8/8/2022 (14m31.417s); 
+cd /mnt/d/Documents/NGS/entrez
+parallel -j 10 --delay 1.1 mydownload :::: wormstaxlist_rerun 2>&1 | tee -a ./wormstaxlist_gb/subsets/log_ncbi_rerun.txt 
+# re-run for species in log_ncbi_ab.txt (after 2022/08/08 re-run)
+# parallel -j 10 --delay 1.1 mydownload :::: wormstaxlist_rerun_ab 2>&1 | tee -a ./wormstaxlist_gb/subsets/log_ncbi_rerun.txt 
+# no errors in log file, so will delete that and wormstaxlist_rerun_ab
+# rm log_ncbi_rerun.txt /mnt/d/Documents/NGS/entrez/wormstaxlist_rerun_ab
+
+# 149 of 150 files are existing and need to be overwritten in their respective folders
+cd /mnt/d/Documents/NGS/entrez/wormstaxlist_gb
+# move all re-run species from log files to their respective folders
+for i in $(ls *_ncbi.gb) 
+do
+spec=$(echo "$i" | sed 's/_/ /;s/_ncbi.gb//') 
+path=$(grep -w "$spec" /mnt/d/Documents/NGS/entrez/wormstaxlist_gb/subsets/subset* | cut -d : -f 1 | sed 's/subsets\///')
+mv "${i}" "${path}"
+done 
+
+# check if last files downloaded per subset reached ~10K index
+cd /mnt/d/Documents/NGS/entrez/wormstaxlist_gb/subsets
+grep "Terschellingia longicaudata" -n subset.aa
+grep "Metschnikowia tuberculata" -n subset.ab
+grep "Diplasterias brucei" -n subset.ac
+grep "Pervagor melanocephalus" -n subset.ad
+grep "Diuronotus aspetos" -n subset.ae
+grep "Abudefduf hoefleri" -n subset.af
+grep "Euphysa japonica" -n subset.ag
+grep "Speleobregma lanzaroteum" -n subset.ah
+grep "Ostrincola japonica" -n subset.ai
+grep "Macroparalepis johnfitchi" -n subset.aj
+grep "Ancistrum crassum" -n subset.ak
+grep "Euphilomedes morini" -n subset.al
+grep "Glabella rosadoi" -n subset.am
+grep "Timoclea scabra" -n subset.an
+grep "Marionia cucullata" -n subset.ao
+grep "Homoieurete macquariense" -n subset.ap
+grep "Cetoscarus ocellatus" -n subset.aq
+grep "Sermyla riquetii" -n subset.ar
+grep "Amathia tertia" -n subset.as
+grep "Philypnodon grandiceps" -n subset.at
+grep "Ophryotrocha urbis" -n subset.au
+grep "Spongilla manconiae" -n subset.av
+grep "Hyalinoecia longibranchiata" -n subset.aw
+# except for subset.ab whose last download was for the 5116th species Metschnikowia tuberculata, all reached expected index
+
+# check that there are no hits for species beyond index 5116 in subset.ab; delete subset.ab_check later 
+tail -n 4884 subset.ab > subset.ab_check
+cd /mnt/d/Documents/NGS/entrez/wormstaxlist_gb/subsets
+parallel -j 10 --delay 1.1 --keep-order 'esearch -db nuccore -query {} | xtract -pattern Count -element Count | xargs -I [] echo "[] {}" >> counts_ab_check' :::: subset.ab_check
+# check how many species with hits were missed in first run (1162 species)
+awk '$1 > 0' counts_ab_check | wc -l
+
+
+
+
+
+# test 
+esearch -db nuccore -query "Amphiporus cruentatus [ORGN] AND (CO1 [GENE] OR COI [GENE] OR COX1 [GENE] OR COXI [GENE])" | xtract -pattern Count -element Count
+
+
+
+
