f41259559677837b7c9488917e140e4805881ef2
dmlicuanan <dmlicuanan@gmail.com>
2022-09-30 14:07:12 +0800

    Added locus-specific primers with overhangs to list

--- nextera_adapters.R
+++ nextera_adapters.R
@@ -3,6 +3,8 @@
 
 # read in library validation and indexing report table from sequencing report
 lib <- read.table("D:/Documents/Repositories/seq_classification/data/library_indexing.txt", col.names = c("sample_no", "sample_name", "conc", "lib_size", "N7XX", "i7", "S5XX", "i5"))
+# exclude indices used for Greg's samples
+lib <- lib[1:34,]
 
 # sequences will be based on https://dnatech.genomecenter.ucdavis.edu/faqs/how-to-prepare-samples-for-multiplexed-amplicon-sequencing-on-illumina-systems/
 # 1) first round PCR primer overhangs 
@@ -10,7 +12,10 @@ lib <- read.table("D:/Documents/Repositories/seq_classification/data/library_ind
 # Reverse overhang P7-tag: 5’ GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG-[locus-specific sequence]
 overhangs <- data.frame(name = c("Nextera_forward_overhang", "Nextera_reverse_overhang"), primer = c("TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG", "GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG"))
 
-# 2) second round PCR primers (Nextera-style index primers where  i5 and i7 indicate the location of the barcode index sequences)
+# 2) primers with overhangs
+full <- read.table("D:/Documents/Repositories/seq_classification/data/primers_overhangs.txt", header = TRUE, col.names = c("name", "primer"))
+
+# 3) second round PCR primers (Nextera-style index primers where  i5 and i7 indicate the location of the barcode index sequences)
 # P5-PCR index primer: 5’ AATGATACGGCGACCACCGAGATCTACAC[i5]TCGTCGGCAGCGTC
 # P7-PCR index primer: 5’ CAAGCAGAAGACGGCATACGAGAT[i7]GTCTCGTGGGCTCGG
 # according to: https://dnatech.genomecenter.ucdavis.edu/wp-content/uploads/2019/03/illumina-adapter-sequences-2019-1000000002694-10.pdf:
@@ -32,7 +37,7 @@ s5$primer <- paste0("AATGATACGGCGACCACCGAGATCTACAC", s5$i5, "TCGTCGGCAGCGTC")
 s5$name <- paste0("Nextera_P5_PCR_index_primer_", s5$S5XX)
 
 # combine tables
-all <- rbind(overhangs, n7[,c(4,3)], s5[,c(4,3)])
+all <- rbind(overhangs, full, n7[,c(4,3)], s5[,c(4,3)])
 
 # load writeFasta() function
 # write .fas file from a vector of IDs (accession numbers info etc.) and sequences
